d19883ec9e8776791357691386d958769de776c7
Eric Ulrich <eulrich@login.curnagl>
2021-10-04 15:16:35 +0200

    fixed cutoff

--- gc_bias/plotting.py
+++ gc_bias/plotting.py
@@ -8,15 +8,15 @@ class Plotting():
         """Plotting 2d histogram for gc content and coverage.
         Cut off for coverage is required because there are some few very high coverage areas
         making the plot useless for low coverage interpretations."""
-        counts,values = np.histogram(df['coverage'])
-        min_count = 200
+        counts,values = np.histogram(df['coverage'],bins=5)
+        min_count = 100
         for i,count in enumerate(counts):
             if count < min_count:
                 break
         cut_off = values[i]
         self.heatmap = px.density_heatmap(df,x='gc_content',y='coverage',\
             marginal_x="histogram",marginal_y="histogram",range_y=[0,cut_off],\
-            nbinsx=20,nbinsy=10)
+                nbinsx=20,nbinsy=100)
 
     def update_labels(self,json_f):
         """This function can be used to update all labels of a plotly figure."""
