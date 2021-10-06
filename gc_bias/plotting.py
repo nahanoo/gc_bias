078a72f08ccb2d9367a7c2fbdbd0961594933cab
Eric Ulrich <eulrich@login.curnagl>
2021-10-06 13:53:44 +0200

    removed cut off,increaded bin size and increased export quality

--- gc_bias/plotting.py
+++ gc_bias/plotting.py
@@ -8,15 +8,10 @@ class Plotting():
         """Plotting 2d histogram for gc content and coverage.
         Cut off for coverage is required because there are some few very high coverage areas
         making the plot useless for low coverage interpretations."""
-        counts,values = np.histogram(df['coverage'],bins=5)
-        min_count = 100
-        for i,count in enumerate(counts):
-            if count < min_count:
-                break
-        cut_off = values[i]
         self.heatmap = px.density_heatmap(df,x='gc_content',y='coverage',\
-            marginal_x="histogram",marginal_y="histogram",range_y=[0,cut_off],\
-                nbinsx=20,nbinsy=100)
+            marginal_x="histogram",marginal_y="histogram",\
+                #First number of size arguments is 16:9 ratio
+                nbinsx=20,nbinsy=20,width=1057.92*0.8,height=595.2*0.8)
 
     def update_labels(self,json_f):
         """This function can be used to update all labels of a plotly figure."""
@@ -27,5 +22,5 @@ class Plotting():
         self.heatmap.update_layout(
             title = labels['title'],
             xaxis_title = labels['xlabel'],
-            yaxis_title= labels['ylabel'],
-            template = labels['theme'])
+            yaxis_title= labels['ylabel']
+            )
