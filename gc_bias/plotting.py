761146dd4ef07b28130f39a2776f9705f19c4120
nahanaoo <ericsven.u@gmail.com>
2021-10-21 14:28:47 +0200

    tuned bins and removed marginal histograms

--- gc_bias/plotting.py
+++ gc_bias/plotting.py
@@ -8,10 +8,8 @@ class Plotting():
         """Plotting 2d histogram for gc content and coverage.
         Cut off for coverage is required because there are some few very high coverage areas
         making the plot useless for low coverage interpretations."""
-        self.heatmap = px.density_heatmap(df,x='gc_content',y='coverage',\
-            marginal_x="histogram",marginal_y="histogram",\
-                #First number of size arguments is 16:9 ratio
-                nbinsx=20,nbinsy=20,width=1057.92*0.8,height=595.2*0.8)
+        self.heatmap = px.density_heatmap(df,x='coverage',y='gc_content',\
+            nbinsy=20,width=1057.92*0.8,height=595.2*0.8)
 
     def update_labels(self,json_f):
         """This function can be used to update all labels of a plotly figure."""
